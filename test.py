bd1a901a17a6b407335f8ad4039a02833c5824f5
gabungas <cristiangabs3@gmail.com>
2025-07-12 16:03:39 -0300

    added better visualization

--- test.py
+++ test.py
@@ -1,20 +1,22 @@
 import pandas as pd
 pd.plotting.register_matplotlib_converters()
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 #The dataset i used = supermarket_sales | source = kaggle 
-Data = pd.read_csv("python pandas\study case 1\supermarket_sales new.csv", index_col=0)
+Market_data = pd.read_csv("python pandas\study case 1\supermarket_sales new.csv", index_col=0, parse_dates= True)
 
 #basic analisys of data
-print(Data.head())
-print(Data.tail())
-print(Data.describe())
+print(Market_data.head())
+print(Market_data.tail())
+print(Market_data.describe())
 
 #parameters for matplot
-x = Data.iloc[:,4]
-y = Data.iloc[:,5] * Data.iloc[:,6] * Data.iloc[:,7] #the price of product times quantity times taxes
+product = Market_data["Product line"]
+income = Market_data.iloc[:,5] * Market_data.iloc[:,6] +( Market_data.iloc[:,5] * Market_data.iloc[:,6] * Market_data.iloc[:,7]) #the price of product times quantity times taxes
 
 plt.title("total selling by categorie")
 plt.figure(figsize=(16,8))
-plt.bar(x,y)
+sns.barplot(data = Market_data, x= income, hue = product)
+
 plt.show()
